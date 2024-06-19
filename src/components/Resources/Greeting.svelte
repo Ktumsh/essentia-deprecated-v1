4614760d3cee2abcc1668a773ef68c14af0204d4
Ktumsh <jbz.january@gmail.com>
2024-06-19 17:41:14 -0400

    try to fix greeting

--- src/components/Resources/Greeting.svelte
+++ src/components/Resources/Greeting.svelte
@@ -1,16 +1,23 @@
 <script>
-  const currentTime = new Date();
-  const currentHour = currentTime.getHours();
+  import { onMount } from "svelte";
 
   let greeting = "";
 
-  if (currentHour >= 5 && currentHour < 12) {
-    greeting = "Buenos días";
-  } else if (currentHour >= 12 && currentHour < 18) {
-    greeting = "Buenas tardes";
-  } else {
-    greeting = "Buenas noches";
-  }
+  onMount(() => {
+    console.log("Component mounted");
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+
+    if (currentHour >= 5 && currentHour < 12) {
+      greeting = "Buenos días";
+    } else if (currentHour >= 12 && currentHour < 18) {
+      greeting = "Buenas tardes";
+    } else {
+      greeting = "Buenas noches";
+    }
+
+    console.log("Greeting set to:", greeting);
+  });
 </script>
 
 {greeting}
