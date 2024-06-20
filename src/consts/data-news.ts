50d6ea54b807a9c47f94956ea1a86f087032dae4
Ktumsh <jbz.january@gmail.com>
2024-06-20 02:36:35 -0400

    try to fix newsGrid

--- src/consts/data-news.ts
+++ src/consts/data-news.ts
@@ -2,7 +2,15 @@ const apiKey = "b1fcdf24b8434bee84d292f9795648e3";
 const url = `https://newsapi.org/v2/everything?q=bienestar&sortBy=popularity&language=es&apiKey=${apiKey}`;
 
 export const fetchNews = async () => {
-  const response = await fetch(url);
-  const newsData = await response.json();
-  return newsData.articles;
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+    const newsData = await response.json();
+    return newsData.articles;
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    return [];
+  }
 };
