182e57bffa07d97d36853bde92da45f4b79c34b2
Ktumsh <jbz.january@gmail.com>
2024-06-25 00:06:27 -0400

    update essentia ai page

--- src/components/HandleAI.svelte
+++ src/components/HandleAI.svelte
@@ -1,7 +1,14 @@
 <script>
-  import ArrowUp from "../icons/arrow-up.svelte";
+  import Dots from "@/icons/svelte/dots.svelte";
+  import ArrowUp from "@/icons/svelte/arrow-up.svelte";
+  import Delete from "@/icons/svelte/delete.svelte";
+  import Edit from "@/icons/svelte/edit.svelte";
   import { getCohereStream } from "@/services/ia";
-  import { onMount } from "svelte";
+  import { onMount, onDestroy } from "svelte";
+  import { scale, fade } from "svelte/transition";
+  import Sidebar from "@/icons/svelte/sidebar.svelte";
+  import NewChat from "@/icons/svelte/new-chat.svelte";
+  import Tooltip from "@/components/Tooltip.svelte";
 
   export let data = {};
 
@@ -15,6 +22,15 @@
   let currentBotMessage = "";
   let chatHistory = [];
   let currentSessionName = "";
+  let openMenuId = null;
+  let menuPosition = { top: 0, left: 0 };
+  let selectedSessionId = "";
+  let shouldAnimateOut = true;
+  let preventClose = false;
+  let editingSessionId = null;
+  let showModal = false;
+  let chatToDelete = null;
+  let sidebarOpen = false;
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -37,6 +53,8 @@
         finalizeBotMessage();
         typing = false;
         isSending = false;
+
+        saveChatHistory();
       } catch (error) {
         console.error("Error fetching AI response:", error);
         addMessage("Hubo un error al obtener la respuesta de la IA.", "bot");
@@ -82,6 +100,24 @@
     if (messages.length > 0 && messages[messages.length - 1].typing) {
       messages[messages.length - 1].typing = false;
     }
+    if (!selectedSessionId) {
+      selectedSessionId = generateId();
+    }
+
+    const currentSessionIndex = chatHistory.findIndex(
+      (session) => session.sessionId === selectedSessionId
+    );
+
+    if (
+      currentSessionIndex !== -1 &&
+      chatHistory[currentSessionIndex].name === "Nuevo chat"
+    ) {
+      const firstUserMessage = generateChatNameFromFirstMessage(messages);
+      if (firstUserMessage) {
+        chatHistory[currentSessionIndex].name = firstUserMessage;
+      }
+    }
+
     saveChatHistory();
   }
 
@@ -90,18 +126,28 @@
   }
 
   function saveChatHistory() {
-    const sessionId = generateId();
-    if (!currentSessionName) {
-      currentSessionName = generateChatNameFromFirstMessage(messages);
-    }
     const now = new Date();
     const dateTime = `${now.toLocaleDateString("es-ES")} ${now.toLocaleTimeString("es-ES", { hour: "2-digit", minute: "2-digit" })}`;
-    chatHistory.push({
-      sessionId,
-      name: currentSessionName,
-      dateTime,
-      messages,
-    });
+
+    const existingSessionIndex = chatHistory.findIndex(
+      (session) => session.sessionId === selectedSessionId
+    );
+
+    if (existingSessionIndex !== -1) {
+      chatHistory[existingSessionIndex].messages = messages;
+      chatHistory[existingSessionIndex].dateTime = dateTime;
+    } else {
+      if (!currentSessionName) {
+        currentSessionName = generateChatNameFromFirstMessage(messages);
+      }
+      chatHistory.push({
+        sessionId: selectedSessionId,
+        name: currentSessionName,
+        dateTime,
+        messages,
+      });
+    }
+
     localStorage.setItem("chatHistory", JSON.stringify(chatHistory));
   }
 
@@ -113,6 +159,7 @@
   }
 
   function selectChatSession(sessionId) {
+    selectedSessionId = sessionId;
     const selectedSession = chatHistory.find(
       (session) => session.sessionId === sessionId
     );
@@ -120,6 +167,125 @@
       messages = selectedSession.messages;
     }
   }
+
+  const toggleOptions = (sessionId, event) => {
+    preventClose = true;
+    const chatMenu = document.querySelector("#chat-menu");
+    const chatItem = event.target.closest(".chat-item");
+    const optionsDot = event.target.closest(".options-dot");
+    if (openMenuId === sessionId) {
+      chatItem.classList.remove("selected");
+      optionsDot.classList.remove("selected");
+      openMenuId = null;
+    } else {
+      if (openMenuId !== null) {
+        const previousSelectedChatItem = document.querySelector(
+          ".chat-item.selected"
+        );
+        const previousSelectedOptionsDot = document.querySelector(
+          ".options-dot.selected"
+        );
+        if (previousSelectedChatItem) {
+          previousSelectedChatItem.classList.remove("selected");
+        }
+        if (previousSelectedOptionsDot) {
+          previousSelectedOptionsDot.classList.remove("selected");
+        }
+      }
+      chatItem.classList.add("selected");
+      optionsDot.classList.add("selected");
+      openMenuId = sessionId;
+      const buttonRect = event.target.getBoundingClientRect();
+      menuPosition = {
+        top: buttonRect.top + chatMenu.offsetTop - 140,
+        left: buttonRect.left + buttonRect.width + window.scrollX - 40,
+      };
+    }
+  };
+
+  const startEditChatName = (sessionId) => {
+    editingSessionId = sessionId;
+    openMenuId = null;
+    originalChatName =
+      chatHistory.find((session) => session.sessionId === sessionId)?.name ||
+      "";
+  };
+
+  const saveChatName = (sessionId, newName) => {
+    if (!newName.trim()) {
+      newName = originalChatName;
+    }
+    chatHistory = chatHistory.map((session) =>
+      session.sessionId === sessionId ? { ...session, name: newName } : session
+    );
+    localStorage.setItem("chatHistory", JSON.stringify(chatHistory));
+    editingSessionId = null;
+  };
+
+  const confirmDeleteChat = (sessionId) => {
+    chatToDelete = sessionId;
+    showModal = true;
+  };
+
+  const deleteChat = () => {
+    chatHistory = chatHistory.filter(
+      (session) => session.sessionId !== chatToDelete
+    );
+    localStorage.setItem("chatHistory", JSON.stringify(chatHistory));
+    showModal = false;
+    chatToDelete = null;
+  };
+
+  const closeModal = () => {
+    showModal = false;
+    chatToDelete = null;
+  };
+
+  const handleClickOutside = (event) => {
+    if (!preventClose) {
+      const menu = document.querySelector(".chat-options");
+      if (menu && !menu.contains(event.target) && openMenuId) {
+        const chatItem = document.querySelector(".chat-item.selected");
+        const optionsDot = document.querySelector(".options-dot.selected");
+        if (chatItem) chatItem.classList.remove("selected");
+        if (optionsDot) optionsDot.classList.remove("selected");
+        shouldAnimateOut = true;
+        openMenuId = null;
+      }
+      if (editingSessionId && !event.target.closest(".edit-name-input")) {
+        editingSessionId = null;
+      }
+    }
+    preventClose = false;
+  };
+
+  const toggleSidebar = () => {
+    sidebarOpen = !sidebarOpen;
+  };
+
+  const createNewChat = () => {
+    const newChat = {
+      sessionId: generateId(),
+      name: "Nuevo chat",
+      dateTime: new Date().toLocaleString("es-ES"),
+      messages: [
+        {
+          id: generateId(),
+          text: formatTextToHTML(
+            "Hola! Cuéntame, ¿en qué puedo ayudarte hoy? 😊"
+          ),
+          sender: "bot",
+          imageUrl: "",
+        },
+      ],
+    };
+    chatHistory = [newChat, ...chatHistory];
+    localStorage.setItem("chatHistory", JSON.stringify(chatHistory));
+    selectChatSession(newChat.sessionId);
+    messages = newChat.messages;
+    selectedSessionId = newChat.sessionId;
+    currentSessionName = "";
+  };
 
   function generateChatNameFromFirstMessage(messages) {
     const firstUserMessage = messages.find(
@@ -162,6 +328,7 @@
   }
 
   onMount(() => {
+    document.addEventListener("mousedown", handleClickOutside);
     loadChatHistory();
     const imgEl = document.querySelector("#user-image");
     if (imgEl) {
@@ -175,11 +342,41 @@
       );
     }, 1000);
   });
+
+  onDestroy(() => {
+    document.removeEventListener("mousedown", handleClickOutside);
+  });
 </script>
 
-<div
-  class="message-list-container flex-grow size-full min-h-[68vh] p-5 overflow-y-auto scroll-smooth"
->
+{#if !sidebarOpen}
+  <div
+    class="flex flex-row items-center justify-center fixed top-20 left-[1px] p-3 rounded-r-xl bg-white dark:bg-base-dark shadow-medium origin-left slide-animation-in"
+  >
+    <Tooltip
+      text="Abrir barra lateral"
+      position="-translate-x-6"
+      origin="origin-top-left"
+      delay="delay-150"
+    >
+      <button
+        on:click={toggleSidebar}
+        class="h-10 rounded-xl px-2 text-base-color-h dark:text-base-color-dark-h hover:bg-gray-200 dark:hover:bg-base-full-dark"
+      >
+        <Sidebar />
+      </button>
+    </Tooltip>
+    <Tooltip text="Nuevo chat" position="-translate-x-1/2" delay="delay-150">
+      <button
+        on:click={createNewChat}
+        class="h-10 rounded-xl px-2 text-base-color-h dark:text-base-color-dark-h hover:bg-gray-200 dark:hover:bg-base-full-dark"
+      >
+        <NewChat />
+      </button>
+    </Tooltip>
+  </div>
+{/if}
+
+<div class="message-list-container flex-grow size-full min-h-[67.5vh] p-5">
   {#if loading}
     <ul class="flex flex-col h-full absolute inset-0">
       <li
@@ -195,9 +392,7 @@
   {:else}
     <ul class="flex flex-col h-full">
       {#each messages as message (message.id)}
-        <li
-          class="group flex flex-col mx-1 px-1 py-2 message {message.sender} "
-        >
+        <li class="group flex flex-col mx-1 px-1 py-2 message {message.sender}">
           <span
             class="user-image flex items-center justify-center size-8 rounded-full bg-base-full-dark font-xs"
           >
@@ -235,7 +430,7 @@
     <input
       id="message-input"
       autocomplete="off"
-      class="flex-grow py-2 px-5 mr-2 border-none outline-none bg-transparent"
+      class="flex-grow py-2 px-5 mr-2 border-none outline-none bg-transparent text-base-color dark:text-base-color-dark"
       placeholder="Escribe tu mensaje aquí..."
     />
     <button
@@ -247,48 +442,170 @@
       <ArrowUp />
     </button>
   </div>
-  {#if chatHistory.length > 0}
-    <div
-      class="hidden absolute left-0 -top-20 ml-5 lg:flex flex-col max-w-64 h-[calc(100dvh-92px)] items-start p-3 border border-gray-100/50 dark:border-base-full-dark-50 bg-black/10 dark:bg-none dark:bg-base-full-dark-40 backdrop-blur backdrop-saturate-150 rounded-xl shadow-md font-motivasans text-base-color dark:text-base-color-dark"
-    >
-      <div
-        class="bg-noise bg-repeat bg-[length:100px] pointer-events-none absolute inset-0 opacity-5"
-      ></div>
-      <div
-        class="flex items-center justify-center w-full h-10 mb-4 bg-black/5 dark:bg-base-full-dark-30 rounded-lg border border-gray-100/50 dark:border-base-full-dark-50"
+  <div
+    id="chat-menu"
+    class="{sidebarOpen
+      ? ''
+      : 'closed'} transform transition-transform fixed left-0 top-0 sm:top-20 flex flex-col w-full min-w-64 sm:max-w-64 h-full sm:h-[calc(100%-80px)] items-start p-3 border border-gray-100/50 dark:border-base-full-dark-50 bg-white dark:bg-base-dark backdrop-blur backdrop-saturate-150 sm:rounded-r-xl shadow-medium font-motivasans text-base-color dark:text-base-color-dark z-[1000] sm:z-0"
+  >
+    <div class="flex items-center justify-between w-full h-10 mb-4">
+      <Tooltip
+        text="Cerrar barra lateral"
+        position="-translate-x-6"
+        origin="origin-top-left"
+        delay="delay-150"
       >
-        <h3 class="w-full text-base font-medium text-center">
-          Chats anteriores
-        </h3>
-      </div>
+        <button
+          on:click={toggleSidebar}
+          class="h-10 rounded-xl px-2 text-base-color-h dark:text-base-color-dark-h hover:bg-gray-200 dark:hover:bg-base-full-dark"
+        >
+          <Sidebar />
+        </button>
+      </Tooltip>
+      <Tooltip text="Nuevo chat" position="-translate-x-1/2" delay="delay-150">
+        <button
+          on:click={createNewChat}
+          class="h-10 rounded-xl px-2 text-base-color-h dark:text-base-color-dark-h hover:bg-gray-200 dark:hover:bg-base-full-dark"
+        >
+          <NewChat />
+        </button>
+      </Tooltip>
+    </div>
 
-      <div class="inner-scrollbar w-full h-full overflow-y-scroll">
+    <div class="relative inner-scrollbar w-full h-full overflow-y-scroll">
+      {#if chatHistory.length > 0}
         {#each chatHistory as session}
-          <button
-            on:click={() => selectChatSession(session.sessionId)}
-            class="flex flex-col w-full text-sm px-3 mb-4 gap-1 hover:brightness-90"
+          <div
+            class="chat-item group relative w-full p-2 gap-1 text-base-color-h dark:text-base-color-dark-h hover:text-base-color dark:hover:text-white hover:bg-gray-200 dark:hover:bg-base-full-dark rounded-xl overflow-hidden {session.sessionId ===
+            selectedSessionId
+              ? 'active'
+              : ''}"
           >
+            {#if editingSessionId === session.sessionId}
+              <input
+                type="text"
+                class="edit-name-input w-full text-sm bg-transparent outline-none border-none"
+                bind:value={session.name}
+                on:blur={() => saveChatName(session.sessionId, session.name)}
+                on:keydown={(e) => e.key === "Enter" && e.target.blur()}
+              />
+            {:else}
+              <button
+                on:click={() => selectChatSession(session.sessionId)}
+                class="flex flex-col w-full text-sm"
+              >
+                <div class="w-full text-ellipsis text-start line-clamp-2">
+                  {session.name}
+                </div>
+                <span
+                  class="text-[10px] text-base-color-m dark:text-base-color-dark-m"
+                >
+                  {session.dateTime}
+                </span>
+                <div
+                  class="absolute bottom-0 top-0 to-transparent right-0 bg-gradient-to-l from-white dark:from-base-dark group-hover:from-gray-200 dark:group-hover:from-base-full-dark w-12 from-0% group-hover:from-60% group-hover:w-12 group-[.selected]:from-60% group-[.selected]:from-gray-200 dark:group-[.selected]:from-base-full-dark group-[.active]:from-60% group-[.active]:from-gray-200 dark:group-[.active]:from-base-full-dark"
+                ></div>
+              </button>
+            {/if}
             <div
-              title={session.name}
-              class="w-full max-w-64 text-ellipsis text-start line-clamp-2"
+              class="options-dot absolute bottom-0 top-0 items-center gap-1.5 pr-2 right-0 flex sm:hidden group-hover:flex group-[.selected]:flex group-[.active]:flex"
             >
-              {session.name}
+              <button
+                on:click={(event) => toggleOptions(session.sessionId, event)}
+                class="button-options group flex items-center justify-center size-[18px] transition dark:hover:text-white"
+                type="button"
+                aria-haspopup="menu"
+                aria-expanded={openMenuId === session.sessionId}
+                data-state={openMenuId === session.sessionId
+                  ? "open"
+                  : "closed"}
+              >
+                <Dots />
+              </button>
             </div>
-            <span
-              class="w-full text-end text-[10px] text-base-color-m dark:text-base-color-dark-m"
-              >{session.dateTime}</span
-            >
-          </button>
+          </div>
         {/each}
+      {:else}
+        <div class="h-full flex items-center justify-center px-2">
+          <p class="text-base-color-h dark:text-base-color-dark-h text-center">
+            Comienza una conversación para que tu historial aparezca aquí...
+          </p>
+        </div>
+      {/if}
+    </div>
+    {#if openMenuId}
+      <div
+        class="origin-top-right sm:origin-top-left p-1 text-sm text-nowrap shadow-medium text-base-color-h dark:text-base-color-dark-h bg-white dark:bg-base-full-dark rounded-xl -translate-x-36 sm:translate-x-4 translate-y-40 sm:translate-y-0"
+        style="top: {menuPosition.top}px; left: {menuPosition.left}px; position: absolute;"
+        in:scale={{ duration: 250 }}
+        out:scale={{ duration: 250 }}
+        on:outstart={(e) =>
+          shouldAnimateOut ? {} : e.stopImmediatePropagation()}
+      >
+        <div
+          class="chat-options flex flex-col p-1"
+          in:fade={{ duration: 250 }}
+          out:fade={{ duration: 250 }}
+          on:outstart={(e) =>
+            shouldAnimateOut ? {} : e.stopImmediatePropagation()}
+        >
+          <button
+            on:click={() => startEditChatName(openMenuId)}
+            class="flex flex-row items-center gap-2 px-2 py-1.5 hover:bg-gray-200 dark:hover:bg-base-dark rounded-xl transition duration-150"
+          >
+            <Edit />
+            Cambiar el nombre
+          </button>
+          <button
+            on:click={() => confirmDeleteChat(openMenuId)}
+            class="flex flex-row items-center gap-2 px-2 py-1.5 text-bittersweet-500 dark:text-cerise-red-600 hover:bg-gray-200 dark:hover:bg-base-dark rounded-xl transition duration-150"
+          >
+            <Delete />
+            Eliminar
+          </button>
+        </div>
+      </div>
+    {/if}
+  </div>
+</form>
+{#if showModal}
+  <button
+    class="modal-backdrop fixed top-0 left-0 w-screen h-screen bg-black/50 z-[9998]"
+    on:click={closeModal}
+    in:fade={{ duration: 150 }}
+    out:fade={{ duration: 150 }}
+  ></button>
+  <div
+    in:scale={{ duration: 150, start: 0.6 }}
+    out:scale={{ duration: 150, start: 0.6 }}
+    class="modal fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-base-color-h dark:text-base-color-dark-h bg-white dark:bg-base-dark rounded-2xl z-[9999] flex flex-col items-center"
+  >
+    <div
+      class="flex items-center justify-between px-5 pb-5 pt-5 sm:p-6 border-b border-black/10 dark:border-white/10"
+    >
+      <h2 class="text-lg font-semibold leading-6">¿Deseas eliminar el chat?</h2>
+    </div>
+    <div class="flex-grow overflow-y-auto p-4 sm:p-6">
+      <div class="flex flex-col gap-3 sm:flex-row-reverse">
+        <button
+          on:click={deleteChat}
+          class="inline-flex flex-shrink-0 items-center justify-center px-4 py-2 leading-5 text-sm font-medium bg-cerise-red-600 hover:brightness-90 rounded-full"
+          ><span>Eliminar</span>
+        </button>
+        <button
+          on:click={closeModal}
+          class="inline-flex flex-shrink-0 items-center justify-center px-4 py-2 leading-5 text-sm font-medium border border-white/10 hover:brightness-90 rounded-full"
+          >Cancelar</button
+        >
       </div>
     </div>
-  {/if}
-</form>
+  </div>
+{/if}
 
 <style>
   .inner-scrollbar::-webkit-scrollbar {
-    width: 6px; /* Chrome, Safari, and Opera */
-    height: 6px; /* Chrome, Safari, and Opera */
+    width: 6px;
+    height: 6px;
   }
 
   .inner-scrollbar::-webkit-scrollbar-thumb {
@@ -297,7 +614,7 @@
   }
 
   .inner-scrollbar-container {
-    padding-right: 6px; /* Reserve space for scrollbar */
+    padding-right: 6px;
   }
 
   .message {
@@ -314,5 +631,35 @@
   button:disabled {
     opacity: 0.6;
     pointer-events: none;
+  }
+  .button-options {
+    cursor: pointer;
+  }
+
+  .chat-item.selected,
+  .chat-item.active {
+    @apply bg-gray-200;
+  }
+
+  .chat-item.selected,
+  .chat-item.active {
+    @apply dark:bg-base-full-dark;
+  }
+
+  #chat-menu.closed {
+    @apply -translate-x-full;
+  }
+
+  .slide-animation-in {
+    animation: slide-in 0.3s ease;
+  }
+
+  @keyframes slide-in {
+    0% {
+      transform: translateX(-100%);
+    }
+    100% {
+      transform: translateX(0);
+    }
   }
 </style>
---
+++ src/components/Tooltip.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { scale, fade } from "svelte/transition";
+  export let text = "";
+  export let position = "";
+  export let origin = "";
+  export let delay = "";
+</script>
+
+<div class="group relative flex flex-col items-center justify-center">
+  <slot></slot>
+  <div
+    class="tooltip-text absolute top-full left-1/2 -translate-y-2 mt-3 whitespace-nowrap text-xs z-10 px-2.5 py-1 rounded-medium shadow-small bg-white dark:bg-base-full-dark text-base-color-h dark:text-base-color-dark-h opacity-0 invisible scale-75 transition group-hover:visible group-hover:opacity-100 group-hover:scale-100 {position} {origin} {delay}"
+  >
+    {text}
+  </div>
+</div>
