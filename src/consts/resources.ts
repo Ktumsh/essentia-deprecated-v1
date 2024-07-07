d20d19d1f315107ff05e6b33a49f8a438d3d63c0
Ktumsh <jbz.january@gmail.com>
2024-07-06 23:47:06 -0400

    update images

--- src/consts/resources.ts
+++ src/consts/resources.ts
@@ -13,7 +13,7 @@ export const RESOURCES: Resources[] = [
     intro: `Encuentra una amplia variedad de consejos sobre <strong class="text-cerise-red-800 dark:text-cerise-red-300 font-medium">hábitos saludables</strong> hasta métodos para <strong class="text-cerise-red-800 dark:text-cerise-red-300 font-medium">potenciar tu salud mental y emocional</strong>, encontrarás recursos variados para impulsar tu bienestar en todos los aspectos de la vida...`,
     quote:
       "El mayor tesoro es la salud, y el conocimiento es la llave para preservarla.",
-    image: "/extras/salud.webp",
+    image: "/extras/salud-600x400.webp",
     imageFull: "/extras/salud-1920x1280.webp",
     background: "bg-resource-1",
     resource: "salud-y-bienestar",
@@ -26,7 +26,7 @@ export const RESOURCES: Resources[] = [
     intro: `Aprende a abordar nuevos deportes, <strong class="text-cerise-red-800 dark:text-cerise-red-300 font-medium">trabajar en tu propio físico</strong> y llevar una vida más activa, adaptada a tus necesidades. Mejorarás tu rendimiento en las actividades diarias y <strong class="text-cerise-red-800 dark:text-cerise-red-300 font-medium">te sentirás más enérgico</strong>...`,
     quote:
       "El movimiento es una medicina para crear el cambio físico, emocional y mental.",
-    image: "/extras/ejercicio.webp",
+    image: "/extras/ejercicio-600x400.webp",
     imageFull: "/extras/ejercicio-1920x1280.webp",
     background: "bg-resource-2",
     resource: "ejercicios-y-fitness",
@@ -38,7 +38,7 @@ export const RESOURCES: Resources[] = [
     title: "Nutrición y Alimentación",
     intro: `Te brindamos soluciones para una <strong class="text-cerise-red-800 dark:text-cerise-red-300 font-medium">alimentación equilibrada</strong> que te permitirán alcanzar tus objetivos, promoviendo un estilo de vida más <strong class="text-cerise-red-800 dark:text-cerise-red-300 font-medium">saludable y sostenible</strong> a largo plazo...`,
     quote: "Que tu alimento sea tu medicina y que tu medicina sea tu alimento.",
-    image: "/extras/nutricion.webp",
+    image: "/extras/nutricion-600x400.webp",
     imageFull: "/extras/nutricion-1920x1280.webp",
     background: "bg-resource-3",
     resource: "nutricion-y-alimentacion",
@@ -51,7 +51,7 @@ export const RESOURCES: Resources[] = [
     intro: `En esta sección encontrarás consejos, métodos para <strong class="text-cerise-red-800 dark:text-cerise-red-300 font-medium">manejar el estrés</strong> y <strong class="text-cerise-red-800 dark:text-cerise-red-300 font-medium">mejorar la salud mental</strong>, promoviendo así la armonía emocional en tu día a día...`,
     quote:
       "La paz interior comienza en el momento en que eliges no permitir que otra persona o evento controle tus emociones.",
-    image: "/extras/bienestar.webp",
+    image: "/extras/bienestar-600x400.webp",
     imageFull: "/extras/bienestar-1920x1280.webp",
     background: "bg-resource-4",
     resource: "bienestar-emocional",
@@ -64,7 +64,7 @@ export const RESOURCES: Resources[] = [
     intro: `Proveemos información y recursos educativos sobre sexualidad, incluyendo métodos anticonceptivos y <strong class="text-cerise-red-800 dark:text-cerise-red-300 font-medium">consejos de cuidado</strong>, para fomentar el entendimiento y <strong class="text-cerise-red-800 dark:text-cerise-red-300 font-medium">cuidado de la diversidad y salud sexual</strong>...`,
     quote:
       "La educación es el arma más poderosa para cambiar el mundo, incluida nuestra comprensión de la salud sexual.",
-    image: "/extras/sexualidad.webp",
+    image: "/extras/sexualidad-600x400.webp",
     imageFull: "/extras/sexualidad-1920x1280.webp",
     background: "bg-resource-5",
     resource: "salud-y-educacion-sexual",
@@ -77,7 +77,7 @@ export const RESOURCES: Resources[] = [
     intro: `Desde la infancia hasta la vejez, nuestra sección <strong class="text-cerise-red-800 dark:text-cerise-red-300 font-medium">Salud para Todas las Edades</strong> ofrece información relevante y específica para cada etapa de la vida. Encuentra consejos útiles y recursos para cuidar la salud en todas las etapas del desarrollo...`,
     quote:
       "La salud es un regalo, y mantenerla es una de las mayores bendiciones que podemos dar a nosotros mismos en cada etapa de la vida.",
-    image: "/extras/edades.webp",
+    image: "/extras/edades-600x400.webp",
     imageFull: "/extras/edades-1920x1280.webp",
     background: "bg-resource-6",
     resource: "salud-para-todas-las-edades",
