cee2f3517700740dcafbf39cc8504181036422ab
Ktumsh <jbz.january@gmail.com>
2024-07-10 20:04:24 -0400

    update news page styles

--- src/components/News/NewsGridItem.jsx
+++ src/components/News/NewsGridItem.jsx
@@ -55,11 +55,11 @@ const NewsGridItem = ({
       ></a>
       <Image
         removeWrapper
-        className="absolute inset-0 size-full object-cover dark:[mask-image:linear-gradient(rgba(0,_0,_0,_1)_0%,_transparent_90%)] group-hover:scale-105 transition"
+        className="absolute inset-0 size-full object-cover dark:[mask-image:linear-gradient(rgba(0,_0,_0,_1)_0%,_transparent_90%)] rounded-xl group-hover:scale-105 transition z-0"
         src={image}
         alt={title}
       />
-      <div className="absolute inset-0 size-full bg-cover bg-center bg-[linear-gradient(to_top,_rgba(0,_0,_0,_0.95)_0%,_transparent_100%)] dark:bg-none"></div>
+      <div className="absolute inset-0 size-full bg-cover bg-center bg-[linear-gradient(to_top,_rgba(0,_0,_0,_0.7)_0%,_transparent_100%)] dark:bg-none"></div>
       <div className="flex flex-col w-full content-center p-6 lg:p-10">
         <div className="flex flex-col items-start justify-start w-full mb-6 sm:mb-8 gap-2 text-white/80 z-10">
           <span className="text-white/80 text-[10px] sm:text-xs uppercase px-3 sm:py-1 bg-bittersweet-500/40 dark:bg-cerise-red-600/40 backdrop-blur backdrop-saturate-150 border border-bittersweet-400/30 dark:border-cerise-red-600/30 overflow-hidden rounded-full shadow-md">
